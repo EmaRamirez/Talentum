211fe4c79de7236605e7cc9266b31c6e167defeb
Emanuel Ramirez <emanuelramirez169@gmail.com>
2024-10-15 23:03:03 -0300

    se agrego el modelo User

---
+++ Back-end/Models/User.cs
@@ -0,0 +1,34 @@
+namespace talentum
+{
+    public class user
+    {
+        public user()
+        {
+
+        }
+        public user(string name, string lastname, string email, string password, TypeUser type, DateTime date)
+        {
+            this.name = name;
+            this.lastName = lastname;
+            this.email = email;
+            this.password = password;
+            this.typeUser = type;
+            this.dateRegister = date;
+
+        }
+        public int userId { get; set; }
+        public string name { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public enum TypeUser
+        {
+            candidato,
+            consumidor
+        }
+        public TypeUser typeUser { get; set; }
+        public DateTime dateRegister { get; set; }
+
+        //public int mensajeId {get;set;}
+    }
+}
--- Back-end/obj/Debug/net8.0/Back-end.AssemblyInfo.cs
+++ Back-end/obj/Debug/net8.0/Back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7385eeb64dd5533322e963f770c77231b4e446a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdba17ab2b15b3a73ef333e000f8bb1d2401a190")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
