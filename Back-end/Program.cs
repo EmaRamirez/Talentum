d96cfddd28c6bc0410263a0811fc2016b0007190
Emanuel Ramirez <emanuelramirez169@gmail.com>
2024-10-20 22:07:49 -0300

    se agrega la conexion en el program y en el appsetting

--- Back-end/Program.cs
+++ Back-end/Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.EntityFrameworkCore;
+using talentum.Data;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+var connectionString = builder.Configuration.GetConnectionString("TalemtumDb") ?? throw new InvalidOperationException("Connection string  'TalemtumDb' not found");
+builder.Services.AddDbContext<Context>(option => option.UseSqlServer(connectionString));
 var app = builder.Build();
+
+
+
 
 // Configure the HTTP request pipeline.
 
